6314f36f225d9d2282d4b5dc44b12f5b7059c7c2
kazu <kzs321kzs@gmail.com>
2024-05-02 18:55:26 +0900

    押されているキーを一覧表示、長押し時たくさん押されないように変更

--- bginput/bginput.py
+++ bginput/bginput.py
@@ -10,35 +10,51 @@ class kb(Node):
         self.release_pub=self.create_publisher(String,"on_release",10)
         self.press_key=String()
         self.release_key=String()
+        self.pressing=set()
         self.start()
 
     def on_press(self,key):
         try:
-            self.press_key.data=key.char
+            self.press_key.data=str(key.char)
         except AttributeError:
             self.press_key.data=str(key)[4:]
-        print("press   :"+self.release_key.data)
-        self.press_pub.publish(self.press_key)
+        
+        if self.press_key.data not in self.pressing:
+            self.press_pub.publish(self.press_key)
+            self.pressing.add(self.press_key.data)
+
+        self.show()
 
     def on_release(self,key):
         try:
-            self.release_key.data=key.char
+            self.release_key.data=str(key.char)
         except AttributeError:
             self.release_key.data=str(key)[4:]
-        print("release :"+self.release_key.data)
+        
+        try:
+            self.pressing.remove(self.release_key.data)
+        except:
+            pass
+        self.show()
         self.release_pub.publish(self.release_key)
     
     def start(self):
+        print("start listener")
         self.listener=keyboard.Listener(on_press=self.on_press,on_release=self.on_release)
         self.listener.start()
+  
+    def show(self):
+        print("\033[1A\033[Kpressing:"+",".join(self.pressing))
 
 def main():
     rclpy.init()
     node=kb()
-    rclpy.spin(node)
+    try:
+        rclpy.spin(node)
+    except:
+        pass
     node.destroy_node()
     rclpy.shutdown()
 
 if __name__=="__main__":
     main()
-            
